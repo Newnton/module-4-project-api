ab26839e5b86d8f0e4287b3bba1a42a2132e69ca
Newnton <zachedubno@gmail.com>
2017-08-01 12:28:27 -0400

    Update 20170726184632_create_listings.rb

--- db/migrate/20170726184632_create_listings.rb
+++ db/migrate/20170726184632_create_listings.rb
@@ -9,9 +9,7 @@ class CreateListings < ActiveRecord::Migration[5.1]
       t.integer :bathrooms
       t.string :apt_type
       t.string :description
-      t.string :image_url_1
-      t.string :image_url_2
-      t.string :image_url_3
+      t.string :image_url
 
       t.references :user, foreign_key: true
 
